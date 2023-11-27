ddb4bd9f14c8ad7dc8996e5a49e8a5e224cbdab1
patrickojunde-cp <141427768+patrickojunde-cp@users.noreply.github.com>
2023-11-27 13:57:42 +0100

    initial setup

---
+++ target/debug/deps/udemy_rust-3b2986920625531a.d
@@ -0,0 +1,5 @@
+/Users/patrick./Patrick-Store/dev/programs/portfolio-projects/learning-rust/udemy/target/debug/deps/udemy_rust-3b2986920625531a.rmeta: src/main.rs
+
+/Users/patrick./Patrick-Store/dev/programs/portfolio-projects/learning-rust/udemy/target/debug/deps/udemy_rust-3b2986920625531a.d: src/main.rs
+
+src/main.rs:
